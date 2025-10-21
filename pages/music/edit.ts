4f7e32568ec9b7446c9a761b296bca4c75baf7f6
Schlauer-Hax <hax@bbn.family>
2025-10-21 15:35:51 +0200

    change to promise

--- pages/music/edit.ts
+++ pages/music/edit.ts
@@ -411,7 +411,7 @@ appendBody(
                                     const data = await API.getIdByShazamByMusic({ path: { id: id.value } }).then(stupidErrorAlert);
                                     alert("Result: " + JSON.stringify(data));
                                 }),
-                                SecondaryButton("Publish").onClick(async () => {
+                                SecondaryButton("Publish").onPromiseClick(async () => {
                                     const data = await API.getIdByProviderByPublishByMusic({ path: { id: id.value, provider: "ampsuite" } });
                                     alert("Result: " + JSON.stringify(data));
                                 }),
